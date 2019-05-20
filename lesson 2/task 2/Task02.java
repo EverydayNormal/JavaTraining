8fc08044a63893f19bd8c7d2b82012b982e9823b
EverydayNormal <serrah1988@gmail.com>
2019-05-20 11:32:29 +0300

    Added task2 homework

---
+++ lesson 2/task 2/Task02.java
@@ -0,0 +1,15 @@
+
+public class Task02 {
+
+	public static void main(String[] args) {
+		DataTypesTester.testInt();
+		DataTypesTester.testBoolean();
+		DataTypesTester.testByte();
+		DataTypesTester.testChar();
+		DataTypesTester.testDouble();
+		DataTypesTester.testFloat();
+		DataTypesTester.testLong();
+		DataTypesTester.testShort();
+		}
+	}
+
